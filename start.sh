da65136605bb7eb895dad903b0d721c7d7aa9094
ShikariBaaZ <91068760+ShikariBaaZ@users.noreply.github.com>
2022-04-04 19:58:44 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/ShikariBaaZ/Lawless_Calculator.git /Lawless_Calculator
+cd /Calculator-Bot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
